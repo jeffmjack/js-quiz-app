f982528ea5f292601585d6c6c828707eb174f640
Jeff Jackson <jeffmjack@gmail.com>
2014-11-25 12:30:04 -0600

    remove score status alerts

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ $(document).ready(function () {
         userScore += thisScore;
         questionNumber = $(this).prop('name');
 
-        alert ("User's score for this question: " + thisScore + "\n User's total score: " + userScore + "\nand the input name was: " + questionNumber);
+        //alert ("User's score for this question: " + thisScore + "\n User's total score: " + userScore + "\nand the input name was: " + questionNumber);
         questionSwitcher (questionNumber);
     });
 });
